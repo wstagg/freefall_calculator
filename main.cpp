4bb2308d533a82a9f3182d42897da44ebff3c7bd
Wesley Stagg <wes.stagg@outlook.com>
2023-04-01 23:17:30 +0100

    When selecting an object to drop, object name now dynamically appears under the object.

--- main.cpp
+++ main.cpp
@@ -13,13 +13,14 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(1000, 800), "Freefall Calculator");
     
-    /* Creates text  */
+    /*------------------------------------*/
+    /* Creating text to display on screen */
+    /*------------------------------------*/
     sf::Font arial;
     if (!arial.loadFromFile("C:\\Users\\wesleystagg\\source\\repos\\SFMLsquare\\arial.ttf"))
     {
-        std::cout << "Cannot load text file.";
+        std::cout << "Cannot load font file.";
     }
-
     sf::Text welcome;
     welcome.setFont(arial);
 
@@ -27,30 +28,63 @@ int main()
     welcome_text.push_back("Welcome to Freefall Calculator!");
     welcome_text.push_back("Please choose the object you would like to drop....");
 
-    int welcome_vec_size = welcome_text.size()-1;
+    int welcome_vec_size = welcome_text.size()-1; 
+    int next_text{ 0 }; // Used to iterate through the welcome text vector
+
+    // Text for object selection 
+    sf::Vector2f cube_text_pos(230.0, 500.0);
+    sf::Vector2f ball_text_pos(640.0, 500.0);
+
+    sf::Text object_text;
+    object_text.setFont(arial);
+    object_text.setString("CUBE");
+    object_text.setPosition(cube_text_pos);
+   
+
+    /*----------------------------------*/
+    /* Loading in textures for objects  */
+    /*----------------------------------*/
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
     
- 
+    sf::Texture concrete;
+    if (!concrete.loadFromFile("C:\\Users\\wesleystagg\\textures\\concrete_texture.jpg"));
+    {
+        std::cout << "Cannot load concrete texture\n";
+    }
+
+    sf::Texture crate;
+    if (!crate.loadFromFile("C:\\Users\\wesleystagg\\textures\\crate_texture.jpg"))
+    {
+        std::cout << "Cannot load concrete texture\n";
+    }
+
+    sf::Texture backgorund;
+    if (!backgorund.loadFromFile("C:\\Users\\wesleystagg\\textures\\sky_bg.png"))
+    {
+        std::cout << "Cannot load background texture\n";
+    }
+    
+    /*----------------------*/
     /* Creates menu objects */
+    /*----------------------*/
     sf::CircleShape menu_ball;
     menu_ball.setRadius(75);
     menu_ball.setPosition(600, 300);
-    menu_ball.setFillColor(sf::Color::Blue);
     menu_ball.setOutlineColor(sf::Color::White);
     menu_ball.setOutlineThickness(0.0);
-
-
-
+    menu_ball.setTexture(&concrete);
 
     sf::RectangleShape menu_square;
     menu_square.setSize(sf::Vector2f(150.f, 150.f));
-    menu_square.setPosition(100, 300);
-    menu_square.setFillColor(sf::Color::Red);
+    menu_square.setPosition(200, 300);
     menu_square.setOutlineColor(sf::Color::White);
     menu_square.setOutlineThickness(10.0);
+    menu_square.setTexture(&crate);
     
-    
-    int next_text{ 0 };
-    //std::cout << "next text before main " << next_text << std::endl;
+    /*---------------------*/
+    /*   main event loop   */
+    /*---------------------*/
     while (window.isOpen())
     {
         sf::Event event;
@@ -78,28 +112,45 @@ int main()
                 {
                     menu_ball.setOutlineThickness(10.0);
                     menu_square.setOutlineThickness(0.0);
+                    object_text.setString("BALL");
+                    object_text.setPosition(ball_text_pos);
                 }
                 if (event.key.code == sf::Keyboard::Left)
                 {
                     menu_ball.setOutlineThickness(0.0);
                     menu_square.setOutlineThickness(10.0);
+                    object_text.setString("CUBE");
+                    object_text.setPosition(cube_text_pos);
+
                 }
             }
     
         }
+        if (next_text == 0)
+        {
+            welcome.setPosition(165, 300);
+            welcome.setCharacterSize(50);
+        }
+        else 
+        {
+            welcome.setPosition(165, 0);
+            welcome.setCharacterSize(30);
+        }
+        
+        
         welcome.setString(welcome_text[next_text]);
 
-
         window.clear();
+       
         window.draw(welcome);
-        
         if (next_text == 1)
         {
             window.draw(menu_ball);
-            window.draw(menu_square); 
+            window.draw(menu_square);
+            window.draw(object_text);
+            
         }
         window.display();
-
 
     }
     
@@ -107,25 +158,3 @@ int main()
 
     return 0;
 }
-
-/*std::cout << "\nWelcome to the freefall calculator!";
-    int running{ 1 };
-    while (running)
-    {
-        std::cout << "\n\nPlease choose the object you would like to drop...\
-            \nor enter (q) to quit.\n\n(b) Ball\n(c) Cube\n\nEnter the your choice: " ;
-        
-        char start{ get_input_c() };
-
-        if (start == 'b')
-        {
-            ball_drop();
-        }
-        else
-        {
-            std::cout << "Goodbye!\n";
-            running = 0;
-        }
-
-
-    }*/
