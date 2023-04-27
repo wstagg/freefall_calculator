045262a5c4295e58812468b4f52e8dcb4cc49be8
Wesley Stagg <wes.stagg@gmail.com>
2023-04-27 22:06:22 +0100

    Text cursor added to the user input page to show where to type. General tidy up and symbol renaming for better code readability.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 #include "inputs.h"
 #include "ball_drop.h" 
 #include "calculations.h"
@@ -16,7 +17,7 @@ int main()
     window.setFramerateLimit(60);
     
     /*------------------------------------*/
-    /* Creating text to display on screen */
+    /* Create text to display on screen   */
     /*------------------------------------*/
     sf::Font arial;
     if (!arial.loadFromFile("fonts\\arial.ttf"))
@@ -40,7 +41,7 @@ int main()
     int welcome_vec_size = welcome_text.size()-1; 
     
     /* Used to iterate through different pages of the application */
-    int next_text{ 0 };   
+    int next_display{ 0 };   
 
     /* Text for object selection */
     sf::Vector2f cube_text_pos(230.0, 500.0);
@@ -64,7 +65,7 @@ int main()
     sf::String user_input;
 
     /* User input page */
-    int next_input{ 0 }; // changes input selection
+    int next_text_input{ 0 }; // changes input selection
 
     sf::Text get_drop_ht;
     get_drop_ht.setFont(arial);
@@ -74,7 +75,8 @@ int main()
 
     sf::Text input_drop_ht;
     input_drop_ht.setFont(arial);
-    input_drop_ht.setPosition(600, 0);
+    input_drop_ht.setPosition(400, 0);
+    input_drop_ht.setString("I");
     std::string drop_ht_string;
 
     sf::Text get_obj_mass;
@@ -85,12 +87,12 @@ int main()
 
     sf::Text input_obj_mass;
     input_obj_mass.setFont(arial);
-    input_obj_mass.setPosition(600, 300);
+    input_obj_mass.setPosition(400, 300);
     std::string obj_mass_string;
 
-
+    
     /*----------------------------------*/
-    /* Loading in textures for objects  */
+    /* Load in textures for objects     */
     /*----------------------------------*/
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
@@ -113,8 +115,14 @@ int main()
         std::cout << "Cannot load background texture\n";
     }
     
+    sf::Texture cloud_texture;
+    if (!cloud_texture.loadFromFile("textures\\cloud.png"))
+    {
+        std::cout << "Cannot load clout texture\n";
+    }
+    
     /*----------------------*/
-    /* Creates menu objects */
+    /* Create menu objects  */
     /*----------------------*/
     sf::CircleShape menu_ball;
     menu_ball.setRadius(75);
@@ -136,8 +144,13 @@ int main()
     /* Creates menu background */
     /*-------------------------*/
     sf::Sprite background_image;
-    background_image.setScale(1,0.9);
+    background_image.setScale(1,1);
     background_image.setTexture(backgorund_texture);
+    //background_image.setColor(sf::Color::Blue);
+
+    sf::Sprite cloud;
+    cloud.setTexture(cloud_texture);
+    cloud.setScale(0.1, 0.1);
     
     /*---------------------*/
     /*   Main Objects      */
@@ -150,9 +163,12 @@ int main()
     /*--------------------*/
     /*   Time Objects     */
    /*---------------------*/
-    sf::Clock mass_input_time;
-    bool obj_mass_input_clock{ false };
+    sf::Clock mass_input_clock;
+    bool obj_mass_input_clock_start{ false };
 
+    sf::Clock text_cursor_clock;
+    bool text_cursor_clock_start{ false };
+    int switch_text_cursor_colour{ 0 };
 
     /*---------------------*/
     /*   main event loop   */
@@ -175,37 +191,39 @@ int main()
                 if (event.key.code == sf::Keyboard::Space)
                 {
                     /* This changes to the object selection menu */
-                    if (next_text < 1) 
+                    if (next_display < 1) 
                     {
-                        ++next_text;
+                        next_display = 1;
                     }
                     /* Cube chosen */ 
-                    else if (object_chosen == 0 && next_text == 1)
+                    else if (object_chosen == 0 && next_display == 1)
                     {
                             menu_text.setString(obj_selection_txt[0]);
                             menu_text.setCharacterSize(75);                            
                             menu_cube.setOutlineThickness(0);
-                            next_text = 2;
+                            next_display = 2;
                     }
                     /* Ball Chosen */
-                    else if (object_chosen == 1 && next_text == 1)
+                    else if (object_chosen == 1 && next_display == 1)
                     {
                         menu_text.setString(obj_selection_txt[1]);
                         menu_text.setCharacterSize(75);                   
                         menu_ball.setOutlineThickness(0);
-                        next_text = 2;
+                        next_display = 2;
                     }
                     /* Goes to next text input when drop_ht string length is > 0 */
-                    else if ((next_text == 3) && (drop_ht_string.length() > 0) && (next_input == 0))
+                    else if ((next_display == 3) && (drop_ht_string.length() > 0) && (next_text_input == 0))
                     {
-                        next_input = 1;
+                        next_text_input = 1;
                         input_drop_ht.setFillColor(sf::Color::Green);
+                        input_obj_mass.setString("I");
+                        text_cursor_clock_start = true;
                     }
-                    else if ((next_input == 1) && (obj_mass_string.length() > 0))
+                    else if ((next_text_input == 1) && (obj_mass_string.length() > 0))
                     {
-                        mass_input_time.restart();
+                        mass_input_clock.restart();
                         input_obj_mass.setFillColor(sf::Color::Green);
-                        obj_mass_input_clock = true;
+                        obj_mass_input_clock_start = true;
                         
                         //debug use of calculation functions
                         double fall_time{ calculate_free_fall_time(obj_mass, drop_ht) };
@@ -215,18 +233,18 @@ int main()
                 /* Alows user to backaspace to delete text inputs */
                 if (event.key.code == sf::Keyboard::BackSpace)
                 {
-                    if (next_input == 0)
+                    if (next_text_input == 0)
                     { 
                         input_drop_ht.setString(erase_text(drop_ht_string));
                     }
-                    else if (next_input == 1)
+                    else if (next_text_input == 1)
                     {
                         input_obj_mass.setString(erase_text(obj_mass_string));
                     }
                 }
                 if (event.key.code == sf::Keyboard::Right)
                 {
-                    if (next_text == 1)
+                    if (next_display == 1)
                     {
                         menu_ball.setOutlineThickness(10.0);
                         menu_cube.setOutlineThickness(0.0);
@@ -238,7 +256,7 @@ int main()
                 }
                 if (event.key.code == sf::Keyboard::Left)
                 {
-                    if (next_text == 1)
+                    if (next_display == 1)
                     {
                         menu_ball.setOutlineThickness(0.0);
                         menu_cube.setOutlineThickness(10.0);
@@ -249,23 +267,27 @@ int main()
                     }
                 }
             }
-            if (event.type == sf::Event::TextEntered)
+            if ((event.type == sf::Event::TextEntered) && (next_display == 3))
             {
-                if (next_input == 0)
+                if (next_text_input == 0)
                 {
-                    if (event.text.unicode >= 48 && event.text.unicode <=57)
+                    if (event.text.unicode >= 48 && event.text.unicode <= 57)
                     {
-                        //std::cout << "ASCII character typed: " << static_cast<char>(event.text.unicode) << std::endl;
-                        drop_ht_string += event.text.unicode;
+                        //std::cout << "ASCII character typed: " << static_cast<char>(event.text.unicode) << std::endl; 
+                        text_cursor_clock_start = false;
+                        input_drop_ht.setFillColor(sf::Color::White);
+                        drop_ht_string += event.text.unicode;                     
                         input_drop_ht.setString(drop_ht_string);
                         drop_ht = std::stod(drop_ht_string);                       
                     }
                 }
-                else if (next_input == 1)
+                else if (next_text_input == 1)
                 {
                     if (event.text.unicode >= 48 && event.text.unicode <= 57)
                     {
                         //std::cout << "ASCII character typed: " << static_cast<char>(event.text.unicode) << std::endl;
+                        text_cursor_clock_start = false;
+                        input_obj_mass.setFillColor(sf::Color::White);
                         obj_mass_string += event.text.unicode;
                         input_obj_mass.setString(obj_mass_string);
                         obj_mass = std::stod(obj_mass_string);
@@ -274,14 +296,18 @@ int main()
             }
         }
         
+        /*************************************/
+        /****** Display specific events ******/
+        /*************************************/
+        
         /* Changes the font size and text position for menu text */
-        if (next_text == 0)
+        if (next_display == 0)
         {
             menu_text.setPosition(165, 300);
             menu_text.setCharacterSize(50);
             menu_text.setString(welcome_text[0]);
         }
-        else if (next_text == 1)
+        else if (next_display == 1)
         {
             menu_text.setPosition(165, 0);
             menu_text.setCharacterSize(30);
@@ -290,7 +316,7 @@ int main()
 
         /* Changes the y pos of object when it has been selected to make 
            it drop from the screen */
-        if (next_text == 2)
+        else if (next_display == 2)
         {
             float fall_velocity{ 10 };
             menu_text.setPosition(250, 0);
@@ -301,20 +327,77 @@ int main()
             //std::cout << menu_ball_pos.y << std::endl;
             if ((menu_ball_pos.y  > y_res) || (menu_cube_pos.y > y_res))
             {
-                next_text++;
+                next_display = 3;
+                text_cursor_clock_start = true;
             }
         }
-         /* Time Events */
-        if (obj_mass_input_clock)
+        else if (next_display == 3)
         {
-            sf::Time seconds_passed = mass_input_time.getElapsedTime();
-            sf::Time seconds = sf::seconds(2.f);
-            if (seconds_passed > seconds)
+            
+            /*************************/
+            /****** Time Events ******/
+            /*************************/
+            
+            // This handles the delay when the object mass has been inputted to //
+            // allow the text enough time to go green before changing to the    //
+            // object drop animation.                                          //
+            if (obj_mass_input_clock_start)
             {
-                next_text = 4;
+                sf::Time obj_seconds_passed = mass_input_clock.getElapsedTime();
+                sf::Time obj_seconds = sf::seconds(1.f);
+                if (obj_seconds_passed > obj_seconds)
+                {
+                    next_display = 4;
+                }
             }
+            
+            // This handles the text cursor pulsating from white to black. Pulse //
+            // frequency can be changed by editting "text_cursor_pulse_seconds"  //
+            if (text_cursor_clock_start)
+            {
+                sf::Time cursor_time_passed = text_cursor_clock.getElapsedTime();
+                sf::Time text_cursor_pulse_seconds = sf::seconds(0.5f);
+               
+                if ((cursor_time_passed > text_cursor_pulse_seconds) && (switch_text_cursor_colour == 0))
+                {
+                    switch_text_cursor_colour = 1;
+                    
+                    if (next_text_input == 0)
+                    {
+                        input_drop_ht.setFillColor(sf::Color::Black);
+                    }
+                    else if (next_text_input == 1)
+                    {
+                        input_obj_mass.setFillColor(sf::Color::Black);
+                    }
+                    text_cursor_clock.restart();
+                }
+                else if ((cursor_time_passed > text_cursor_pulse_seconds) && (switch_text_cursor_colour == 1))
+                {
+                    switch_text_cursor_colour = 0;
+                    
+                    if (next_text_input == 0)
+                    {
+                        input_drop_ht.setFillColor(sf::Color::White);
+                    }
+                    else if (next_text_input == 1)
+                    {
+                        input_obj_mass.setFillColor(sf::Color::White);
+                    }
+                    text_cursor_clock.restart();
+                }
+                //std::cout << cursor_time_passed.asSeconds() << std::endl;
+                //std::cout << "switch colour: " << switch_colour << std::endl;   
+            }
+            
         }
-
+        else if (next_display == 4)
+        {
+            menu_ball.setPosition(0, 0);
+            menu_cube.setPosition(0, 0);
+        }   
+        /*******************************/
+        
         /*---------------------*/
         /*   Main draw block   */
         /*---------------------*/
@@ -322,13 +405,14 @@ int main()
         //window.draw(background_image);
         
         /* Application welcoms page */
-        if (next_text == 0)
+        if (next_display == 0)
         {
+            //window.draw(cloud);
             window.draw(menu_text);
             window.draw(press_space);   
         }
         // Object selection page
-        else if (next_text == 1)
+        else if (next_display == 1)
         {
             window.draw(menu_text);
             window.draw(menu_ball);
@@ -337,7 +421,7 @@ int main()
             window.draw(press_space);
         }
         /* Object selected page */
-        else if (next_text == 2)
+        else if (next_display == 2)
         {
             window.draw(menu_text);
             if (object_chosen == 0)
@@ -350,20 +434,28 @@ int main()
             }
         }
         /* User input page */
-        else if (next_text == 3)
+        else if (next_display == 3)
         {
             window.draw(press_space);
             window.draw(get_drop_ht);
             window.draw(get_obj_mass);
             window.draw(input_drop_ht);
-            window.draw(input_obj_mass);    
+            window.draw(input_obj_mass);
+            //window.draw(text_cursor);
         } 
-        else if (next_text == 4)
-        {
-            
-            
-                window.draw(press_space);
-            
+        else if (next_display == 4)
+        {                        
+            window.draw(background_image);
+            //window.draw(cloud);
+            if (object_chosen == 0)
+            {
+                window.draw(menu_cube);
+            }
+            else if (object_chosen == 1)
+            {
+                window.draw(menu_ball);
+
+            }
         }
         window.display();
        
